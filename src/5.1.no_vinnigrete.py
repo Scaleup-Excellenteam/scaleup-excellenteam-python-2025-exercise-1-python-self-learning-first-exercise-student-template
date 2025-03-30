352c5ffea9126ba0d3bc03b2ce683a90096d70ed
guyyy1000 <guyyy1000@gmail.com>
2025-03-30 20:14:40 +0300

    no_vinnigrete code

---
+++ src/5.1.no_vinnigrete.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import random
+
+
+def no_vinnigrete(first_date, second_date):
+    """
+    Generates a random date between two given dates.
+
+    :param start_date: A string in the format YYYY-MM-DD representing the start date.
+    :param end_date: A string in the format YYYY-MM-DD representing the end date.
+    :return: A random date string in the format YYYY-MM-DD, and the weekday.
+    """
+    date_format = "%Y-%m-%d"
+
+    try:
+        first_date_ordinal = datetime.strptime(first_date, date_format).toordinal()
+        second_date_ordinal = datetime.strptime(second_date, date_format).toordinal()
+
+        if first_date_ordinal > second_date_ordinal:
+            raise ValueError("The first date must be before the second date.")
+
+        print(first_date_ordinal, second_date_ordinal)
+
+        random_date = datetime.fromordinal(random.randint(first_date_ordinal, second_date_ordinal))
+
+        return random_date.strftime(date_format), random_date.weekday()
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        return None, None
+
+if __name__ == '__main__':
+    start_date = input("Enter first date (YYYY-MM-DD): ")
+    end_date = input("Enter second date (YYYY-MM-DD): ")
+
+    random_date, weekday= no_vinnigrete(start_date, end_date)
+
+    print(f"The random date is: {random_date}")
+    if weekday == 0:  # Monday is represented as value 0 in weekday().
+        print("אין לי ויניגרט!")
