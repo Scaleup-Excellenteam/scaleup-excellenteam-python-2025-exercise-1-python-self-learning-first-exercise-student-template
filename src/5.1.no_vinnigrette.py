88bb9c985d38fc564aba59611d4854aa3b3d6147
RonAdi77 <ronadi789@gmail.com>
2025-03-31 20:20:05 +0300

    Update 5.1.no_vinnigrette.py

--- src/5.1.no_vinnigrette.py
+++ src/5.1.no_vinnigrette.py
@@ -3,29 +3,30 @@ import datetime as dt
 import random as rnd
 
 MONDAY = 0
-def no_vinnigrete():
+def no_vinnigrete(first_date_str,second_date_str):
     """
     Calculate a random date between two inputs dates,
     and then check if the random date is Monday and print "Today is Monday and don't have vinnigrete"
     """
-
-    first_date = input("please enter first date: ")
-    second_date = input("please enter second date: ")
     try:
-        first_date = dt.datetime.strptime(first_date, "%Y-%m-%d").date()
-        second_date = dt.datetime.strptime(second_date, "%Y-%m-%d").date()
+        first_date = dt.datetime.strptime(first_date_str, "%Y-%m-%d").date()
+        second_date = dt.datetime.strptime(second_date_str, "%Y-%m-%d").date()
     except ValueError:
         print("invalid date")
         return None
 
-    # convert both dates to ordinal numbers and exclude lower and upper bounds
-    ordinal_first = min(first_date.toordinal(), second_date.toordinal()) + 1
-    ordinal_second = max(first_date.toordinal(), second_date.toordinal()) - 1
+    if first_date == second_date:
+        random_date = first_date
+    else:
+        # convert both dates to ordinal numbers and exclude lower and upper bounds
+        ordinal_first = min(first_date.toordinal(), second_date.toordinal()) + 1
+        ordinal_second = max(first_date.toordinal(), second_date.toordinal()) - 1
 
-    random_date = rnd.randint(ordinal_first, ordinal_second)
-    random_date = dt.datetime.fromordinal(random_date)
+        random_date = rnd.randint(ordinal_first, ordinal_second)
+        random_date = dt.datetime.fromordinal(random_date)
 
-    print("The random date is: ",random_date.date())
+    print(f"The random date is: {random_date}")
+
     if random_date.weekday() == MONDAY:
         print("Today is Monday and i dont have vinnigrete")
     else:
@@ -34,5 +35,5 @@ def no_vinnigrete():
 
 
 if __name__ == '__main__':
-    no_vinnigrete()
+    no_vinnigrete("2023-07-10", "2023-07-10")
 
