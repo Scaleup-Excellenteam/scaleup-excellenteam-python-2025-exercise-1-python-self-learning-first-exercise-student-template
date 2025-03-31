2514a28cfd2a11eecd2123eb9866d2d539f637c1
Yuval Boker <yuvalboker588@gmail.com>
2025-03-31 19:17:08 +0300

    add long_cat_is_long

---
+++ src/6.3.long_cat_is_long.py
@@ -0,0 +1,23 @@
+def long_cat_is_long(input_text):
+    """
+    Count the length of each word in a text.
+    """
+
+    clean_text = ''.join(char.lower() if char.isalpha() else ' ' for char in input_text)
+    words = clean_text.split()
+    word_lengths = {word: len(word) for word in words}
+
+    return word_lengths
+
+
+if __name__ == "__main__":
+    sample_text = """
+    You see, wire telegraph is a kind of a very, very long cat.
+    You pull his tail in New York and his head is meowing in Los Angeles.
+    Do you understand this?
+    And radio operates exactly the same way: you send signals here, they receive them there.
+    The only difference is that there is no cat.
+    """
+
+    word_count_result = long_cat_is_long(sample_text)
+    print(word_count_result)
