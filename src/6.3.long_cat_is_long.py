4b280f05160f0a9373bdb4437ed9e6b1ec58bf64
RonAdi77 <ronadi789@gmail.com>
2025-04-01 14:09:47 +0300

    Update 6.3.long_cat_is_long.py

--- src/6.3.long_cat_is_long.py
+++ src/6.3.long_cat_is_long.py
@@ -1,21 +1,26 @@
 
-
-def long_cat_is_long(text):
-    """
-        Removes non-alphabetic characters from words in the input text and converts them to lowercase.
-        Returns a dictionary where keys are words and values are their lengths.
-        Ignores empty words resulting from non-alphabetic content.
-        """
-    word_list = [''.join(c for c in word if c.isalpha()) for word in text.lower().split()]
-    return {word : len(word) for word in word_list if word}
-
-
-if __name__ == '__main__':
-    text = """
+"""
+This module defines the `long_cat_is_long` function, which processes text
+to remove non-alphabetic characters from words, converts them to lowercase,
+and returns a dictionary mapping words to their lengths.
+"""
+TEXT = """
     You see, wire telegraph is a kind of a very, very long cat.
     You pull his tail in New York and his head is meowing in Los Angeles.
     Do you understand this?
     And radio operates exactly the same way: you send signals here, they receive them there.
     The only difference is that there is no cat.
     """
-    print(long_cat_is_long(text))
+def long_cat_is_long(text):
+   """
+    Cleans the input text by removing non-alphabetic characters from words and converting them to lowercase.
+    Args:
+        text (str): The input text to process.
+    Returns:
+        dict: A dictionary where keys are cleaned words and values are their lengths.
+    """
+    word_list = [''.join(c for c in word if c.isalpha()) for word in text.lower().split()]
+    return {word : len(word) for word in word_list if word}
+
+if __name__ == '__main__':
+    print(long_cat_is_long(TEXT))
