f6e58b98a1e0be5f68ad4a05eb9d2f2007773462
guyyy1000 <guyyy1000@gmail.com>
2025-04-01 17:52:09 +0300

    5.3.parsle_tongue.py fix comments

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -1,38 +1,42 @@
-"""Extracts hidden messages from a file using a generator. Messages are defined as lowercase English letters (at least 5 characters)
-    followed by an exclamation mark (!)."""
+"""
+Extracts hidden messages from a file using a generator.
+Messages are defined as lowercase English letters (at least 5 characters)
+followed by an exclamation mark (!).
+"""
 
 import os
 import re
 
 def parsle_tongue(file_path: str = None):
     """
-    Extracts hidden messages from a file using a generator. Messages are defined as lowercase English letters (at least 5 characters)
+    Extracts hidden messages from a file using a generator.
+    Messages are defined as lowercase English letters (at least 5 characters)
     followed by an exclamation mark (!).
 
     :param file_path: Path to a file
     :yield: Extracted messages one by one
     """
     if file_path is None:
-        relative_path = './resources/logo.jpg'
-        file_path = os.path.abspath(relative_path)
+        path = './resources/logo.jpg'
+        file_path = os.path.abspath(path)
 
-    PATTERN = re.compile(rb'[a-z]{5,}!') # the hidden messages pattern.
-    CHUNK_SIZE = 1024
+    pattern = re.compile(rb'[a-z]{5,}!') # the hidden messages pattern.
+    chunk_size = 1024
     messages = []
 
     try:
         with open(file_path, 'rb') as file:
             buffer = b''
-            while chunk := file.read(CHUNK_SIZE):
+            while chunk := file.read(chunk_size):
                 buffer += chunk
-                pattern_matches = PATTERN.findall(buffer)
+                pattern_matches = pattern.findall(buffer)
 
                 for match in pattern_matches:
                     messages.append(match.decode()[:-1])
 
                 # keep the last 10 bytes as the beginning of the next chunk in case the message is between chunks.
                 # If the message is in the last 10 bytes so find where it ends.
-                if any(match in PATTERN.findall(buffer[-10:]) for match in pattern_matches):
+                if any(match in pattern.findall(buffer[-10:]) for match in pattern_matches):
                     match_start = buffer.find(match)
                     match_end = match_start + len(match)
                     buffer = buffer[match_end:]
@@ -46,8 +50,8 @@ def parsle_tongue(file_path: str = None):
         return ""
 
 if __name__ == '__main__':
-    relative_path  = './resources/logo.jpg'
-    full_path = os.path.abspath(relative_path)
+    RELATIVE_PATH  = './resources/logo.jpg'
+    FULL_PATH = os.path.abspath(RELATIVE_PATH)
 
     found_messages = parsle_tongue()
     if found_messages:
