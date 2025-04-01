266753724a759bd440d241ed4a50a83a22b96993
RonAdi77 <ronadi789@gmail.com>
2025-04-01 14:05:11 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -1,31 +1,36 @@
+"""
+This module provides functions to interleave multiple iterables, either as a list or a generator.
+Functions:
+    - interleave(*args: Iterable) -> List:
+        Merges multiple iterables and returns a flattened list.
+        
+    - generator_interleave(*args: Iterable) -> Generator:
+        Interleaves multiple iterables and yields elements one by one.
+"""
 from itertools import zip_longest
 from typing import Iterable, List, Generator
 
-
-def interleave(*args:Iterable)->List:
+def interleave(*args: Iterable) -> List:
     """
-     Interleaves multiple iterables and returns a flattened list.
-    :param args: Iterables to merge
-    :return: Flattened list
+    Interleaves multiple iterables and returns a flattened list.
+    :param args: Iterables to merge.
+    :return: Flattened list with interleaved elements.
     """
     return [item for items in zip_longest(*args) for item in items if item is not None]
 
-def generator_interleave(*args:Iterable)->Generator:
+def generator_interleave(*args: Iterable) -> Generator:
     """
-     Interleaves multiple iterables and yields elements one by one.
+    Interleaves multiple iterables and yields elements one by one.
+    
     :param args: One or more iterables to interleave.
-    :return:Elements in interleaved order.
+    :yield: Elements in interleaved order.
     """
-
     for items in zip_longest(*args):
         for item in items:
             if item is not None:
                 yield item
 
-
 if __name__ == '__main__':
-    print(communicating_vessels('abc', [1, 2, 3], ('!', '@', '#')))
-    for i in communicating_vessels_generator('abc', [1, 2, 3], ('!', '@', '#')):
-        print(i,end=" ")
-
-    
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    for i in generator_interleave('abc', [1, 2, 3], ('!', '@', '#')):
+        print(i, end=" ")
