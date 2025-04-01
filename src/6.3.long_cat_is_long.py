13e7a9fdca6ac27ce14c44921990cd279ff664a1
RonAdi77 <ronadi789@gmail.com>
2025-04-01 14:11:54 +0300

    Update 6.3.long_cat_is_long.py

--- src/6.3.long_cat_is_long.py
+++ src/6.3.long_cat_is_long.py
@@ -4,13 +4,6 @@ This module defines the `long_cat_is_long` function, which processes text
 to remove non-alphabetic characters from words, converts them to lowercase,
 and returns a dictionary mapping words to their lengths.
 """
-TEXT = """
-    You see, wire telegraph is a kind of a very, very long cat.
-    You pull his tail in New York and his head is meowing in Los Angeles.
-    Do you understand this?
-    And radio operates exactly the same way: you send signals here, they receive them there.
-    The only difference is that there is no cat.
-    """
 def long_cat_is_long(text):
    """
     Cleans the input text by removing non-alphabetic characters from words and converting them to lowercase.
@@ -23,4 +16,11 @@ def long_cat_is_long(text):
     return {word : len(word) for word in word_list if word}
 
 if __name__ == '__main__':
+    TEXT = """
+    You see, wire telegraph is a kind of a very, very long cat.
+    You pull his tail in New York and his head is meowing in Los Angeles.
+    Do you understand this?
+    And radio operates exactly the same way: you send signals here, they receive them there.
+    The only difference is that there is no cat.
+    """
     print(long_cat_is_long(TEXT))
