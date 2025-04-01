bd7e432a3a6ebc2b1b0d9130d379236219c51cf6
RonAdi77 <ronadi789@gmail.com>
2025-04-01 11:08:22 +0300

    Update 5.2.piece_of_cake.py

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -12,16 +12,6 @@ def piece_of_cake(prices:Dict[str,float] ={}  ,optionals:List[str] = [],**kwargs
         print("KeyError")
         return None
 
-# def piece_of_cake(prices: Dict[str, float] = {}, optionals: List[str] = [], **kwargs) -> float:
-#     """
-#     Computes a weighted sum of values from the given dictionary, based on percentages provided
-#     in keyword arguments, while ignoring optional keys.
-#     """
-#     total_weight = 0.0
-#     for name, percentage in kwargs.items():
-#         if name not in optionals and name in prices:
-#             total_weight += (percentage / 100) * prices[name]
-#     return total_weight
 
 if __name__ == '__main__':
     print(piece_of_cake({'chocolate': 18, 'milk': 8}, coco=200, milk=100))
