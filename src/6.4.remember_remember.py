7be5514a8c525c6bf2149d961fb83750d553d9a4
RonAdi77 <ronadi789@gmail.com>
2025-04-16 20:17:27 +0300

    Update 6.4.remember_remember.py

--- src/6.4.remember_remember.py
+++ src/6.4.remember_remember.py
@@ -19,20 +19,20 @@ def remember_remember(path):
         str: The extracted message, or None if the file is not found.
     """
     try:
-        img = Image.open(path).convert('L')
-        width, height = img.size
-        message = []
-
-        for col in range(width):
-            for row in range(height):
-                pixel = img.getpixel((col, row))
-                if pixel == 1:
-                    message.append(chr(row))
-                    break
+        with img = Image.open(path).convert('L'):
+            width, height = img.size
+            message = []
+    
+            for col in range(width):
+                for row in range(height):
+                    pixel = img.getpixel((col, row))
+                    if pixel == 1:
+                        message.append(chr(row))
+                        break
 
     except FileNotFoundError as e:
-        print(e)
-        return None
+        raise FileNotFoundError(f"The file at {path} was not found.")
+        
 
     return ''.join(message)
 
