c90da21b835531c7c0268aa765f9f48972ae7f94
Basharyam <Bshar.yamin@gmail.com>
2025-04-18 23:27:58 +0300

    Update 5.3.parsle_tongue.py

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -8,34 +8,33 @@ searching for patterns that match these secret messages and yields them one by o
 Raises:
     FileNotFoundError: If the specified file does not exist.
 """
-def parsle_tongue():
+def parsle_tongue(file_path: str):
     """
-    Module to extract secret messages from a binary file by reading its content.
+    Extracts secret messages from a binary file.
 
-    The secret messages are sequences of lowercase letters followed by an exclamation mark ('!').
-    Each message must contain at least five lowercase letters. This function processes a binary file,
-    searching for patterns that match these secret messages and yields them one by one.
+    Parameters:
+        file_path (str): Path to the binary file.
 
-    Raises:
-        FileNotFoundError: If the specified file does not exist.
+    Yields:
+        str: Secret messages that contain at least 5 lowercase letters and end with '!'.
     """
     try:
-        with open("logo.jpg", "rb") as file:
+        with open(file_path, "rb") as file:
             binary_data = file.read().decode("ascii", errors="ignore")
-            new_str=""
-            for bin_text in binary_data:
-                if bin_text.islower():
-                    new_str += bin_text
-                elif bin_text == '!':
+            new_str = ""
+            for char in binary_data:
+                if char.islower():
+                    new_str += char
+                elif char == '!':
                     if len(new_str) >= 5:
                         yield new_str
                     new_str = ""
                 else:
                     new_str = ""
     except FileNotFoundError:
-        print("No such file or directory: ", file)
+        raise FileNotFoundError(f"No such file or directory: {file_path}")
 
 
 if __name__ == "__main__":
-    for parse in parsle_tongue():
+    for parse in parsle_tongue("logo.jpg"):
         print(parse)
