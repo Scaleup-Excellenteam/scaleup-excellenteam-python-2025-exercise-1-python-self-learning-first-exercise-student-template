6d368660b185a06faf42335d5c1d253be1b43a27
Basharyam <Bshar.yamin@gmail.com>
2025-04-18 23:11:07 +0300

    Update 5.1.thats_the_way.py

--- src/5.1.thats_the_way.py
+++ src/5.1.thats_the_way.py
@@ -13,25 +13,22 @@ import os
 
 def thats_the_way(your_path):
     """
-      Lists all files in the specified directory whose names start with "deep".
+    Lists all files in the specified directory whose names start with "deep".
 
-      Parameters:
-      your_path (str): The path to the directory to search for files.
+    Parameters:
+    your_path (str): The path to the directory to search for files.
 
-      Returns:
-      list: A list of file names that start with "deep".
-            If the directory is not found, returns a string message.
-      """
-    try:
-        lst =[]
-        fold=os.listdir(your_path)
-        for f in fold:
-            if f.startswith("deep"):
-                lst.append(f)
-        return lst
-    except FileNotFoundError:
-        return "File not found"
+    Returns:
+    list: A list of file names that start with "deep".
+
+    Raises:
+    FileNotFoundError: If the specified directory does not exist.
+    """
+    if not os.path.isdir(your_path):
+        raise FileNotFoundError(f"The directory '{your_path}' was not found.")
+
+    return [f for f in os.listdir(your_path) if f.startswith("deep")]
 
 
-if __name__ == "__main__":
+if __name__ ==__main__:
     thats_the_way("")
