853217dc1166cff80fffe30840e5bd74857d107c
Basharyam <Bshar.yamin@gmail.com>
2025-04-22 12:41:49 +0300

    Update 5.2.cup_of_join.py

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -1,35 +1,33 @@
 """
 Module for joining elements from multiple iterables with a separator.
-
 This script defines a function that takes multiple iterables and joins their elements
 into a single list. A specified separator is inserted between elements from different iterables.
-The separator can be customized, and the default is an empty string.
+The separator can be customized, and the default is a hyphen '-'.
 """
-def cup_of_join(*lst, sep=''):
+def cup_of_join(*iterables: list, sep: str = '-') -> list:
     """
-        Joins elements from multiple iterables into a single list with a separator.
-
-        Parameters:
-            *lst (iterables): Multiple iterables (lists, tuples, etc.) to join.
-            sep (str): The separator to insert between elements of different iterables (default is an empty string).
-
-        Returns:
-            list: A list containing all elements from the iterables, separated by `sep`.
-                  If no iterables are provided, returns None.
-
-        Example:
-            cup_of_join([1, 2], [8], [9, 5, 6], sep='@') -> [1, 2, '@', 8, '@', 9, 5, 6]
-        """
-    if len(lst)==0:
+    Joins elements from multiple iterables into a single list with a separator.
+    
+    Parameters:
+        *iterables (list): Multiple lists to join.
+        sep (str): The separator to insert between elements of different lists (default is '-').
+    
+    Returns:
+        list: A list containing all elements from the iterables, separated by `sep`.
+              If no iterables are provided, returns None.
+    
+    Example:
+        cup_of_join([1, 2], [8], [9, 5, 6], sep='@') -> [1, 2, '@', 8, '@', 9, 5, 6]
+    """
+    if len(iterables) == 0:
         return None
-    new_lst=[]
-    for item in lst:
-        for num in item:
-            new_lst.append(num)
-        if  sep !='':
-            new_lst.append(sep)
-    return new_lst
+    joined_result = []
+    for index, current_iterable in enumerate(iterables):
+        if index != 0 and sep != '':
+            joined_result.append(sep)
+        joined_result.extend(current_iterable)
+    return joined_result
 
 
 if __name__ == "__main__":
-    print(cup_of_join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(cup_of_join([1, 2], [8], [9, 5, 6], sep='@'))  
